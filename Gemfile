b0b92549a89c8f00e6f84eba52a5d8b87233adbe
sotirelisc <gtakriz@hotmail.com>
2016-02-14 09:04:33 +0000

    Basic Parse connection & Venues retrieval.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'parse-ruby-client', git: 'https://github.com/adelevie/parse-ruby-client.git'
 group :development, :test do
   gem 'byebug'
 end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,17 @@
+require 'parse-ruby-client'
+
 class VisitorsController < ApplicationController
+  before_filter :init_parse
+  
+  def index
+    venues_query = @parse.query("Rate")
+    @venues = venues_query.get
+  end
+  
+  private
+  
+    def init_parse
+      @parse = Parse.create :application_id => "FXbSp2Zxvbf48HmBB1IT2hCRDH9ndrJsgy8ZXEaA",
+                            :api_key        => "9R6QlLNW9cYclCj434QHTVltzDM0PDvb38BXAl0h"
+    end
 end
